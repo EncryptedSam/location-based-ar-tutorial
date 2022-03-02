df9c1b33b47855f1101effd62d77580132608d39
Sampath Kumar Ajjapaga <sampathkumar.ajjapaga@gmail.com>
2022-03-02 13:12:38 +0530

    script

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-// window.onload = () => {
-//     let places = staticLoadPlaces();
-//     renderPlaces(places);
-// };
-
-// function staticLoadPlaces() {
-//     return [
-//         {
-//             name: 'Magnemite',
-//             location: {
-//                 lat: 17.3612304,
-//                 lng: 78.3872802,
-//             }
-//         },
-//     ];
-// }
-
-// function renderPlaces(places) {
-//     let scene = document.querySelector('a-scene');
-
-//     places.forEach((place) => {
-//         let latitude = place.location.lat;
-//         let longitude = place.location.lng;
-
-//         let model = document.createElement('a-entity');
-//         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-//         model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-//         model.setAttribute('rotation', '0 180 0');
-//         model.setAttribute('animation-mixer', '');
-//         model.setAttribute('scale', '0.5 0.5 0.5');
-
-//         model.addEventListener('loaded', () => {
-//             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-//         });
-
-//         scene.appendChild(model);
-//     });
-// }
-
-
-// -----------
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
     button.innerText = '﹖';
@@ -81,6 +40,7 @@ var models = [
 ];
 
 var modelIndex = 0;
+
 var setModel = function (model, entity) {
     if (model.scale) {
         entity.setAttribute('scale', model.scale);
